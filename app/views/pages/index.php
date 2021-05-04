8521fa313f97fad3d907395c56fbb24f47b5b1af
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-05-04 19:24:31 +0000

    finished working on landing page front

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -6,7 +6,7 @@
     Featured
   </div>
   <div class="card-body">
-    <h5 class="card-title">Who we are ?</h5>
+    <h5 class="card-title">Who are we ?</h5>
     <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis illo voluptates architecto aliquam distinctio vitae optio sint pariatur provident in, odit tenetur nostrum sapiente fuga accusantium voluptas fugit blanditiis quibusdam!
     Dignissimos officiis deleniti, dolorem atque laborum excepturi totam, ex earum numquam sit quisquam iste non? Quod magnam doloribus maiores, totam, fugit ad quam tempora excepturi veritatis saepe cum repellat nobis.
     Vero adi</p>
@@ -20,4 +20,83 @@
   <div class="card-header">
     GALLERY
   </div>
+
+</div>
+<!-- Gallery -->
+<div class="row">
+  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+    <img
+      src="./public/img/istockphoto-1000887536-612x612.jpg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+
+    <img
+    src="./public/img/w555mh8d5vjlzk18skxj.webp"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+    src="./public/img/images.jpg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+
+    <img
+    src="./public/img/www.usnews.com.jpg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+  </div>
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+    src="./public/img/imagees.jpg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+
+    <img
+    src="./public/img/UNI330896.JPG"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+  </div>
+</div>
+
+  
+
+<!-- Gallery -->
+<div class="card card-container text-center my-sm-3">
+  <div class="card-header">
+    Our Curriculums
+  </div>
+  <div class="cards d-flex justify-content-around">
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">find out more ?</a>
+  </div>
+</div>
+<div class="card " style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">find out more ?</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">find out more ?</a>
+  </div>
+  </div>
+ 
+</div>
+</div>
+  
   <?php require APPROOT .'/views/inc/footer.php'; ?>
