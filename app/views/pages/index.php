0ef824ce3e1bae5074b25369e8497c52909bf800
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-05-04 15:32:58 +0000

    more updates on landing page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,23 @@
 <?php require APPROOT .'/views/inc/header.php'; ?>
-
+<?php require APPROOT .'/views/inc/nav.php'; ?>
+<div class="container">
+<div class="card text-center my-sm-3">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Who we are ?</h5>
+    <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis illo voluptates architecto aliquam distinctio vitae optio sint pariatur provident in, odit tenetur nostrum sapiente fuga accusantium voluptas fugit blanditiis quibusdam!
+    Dignissimos officiis deleniti, dolorem atque laborum excepturi totam, ex earum numquam sit quisquam iste non? Quod magnam doloribus maiores, totam, fugit ad quam tempora excepturi veritatis saepe cum repellat nobis.
+    Vero adi</p>
+    <a href="#" class="btn btn-primary my-sm-3">contact us</a>
+  </div>
+ 
+</div>
+</div>
+<div class="container">
+<div class="card text-center my-sm-3">
+  <div class="card-header">
+    GALLERY
+  </div>
   <?php require APPROOT .'/views/inc/footer.php'; ?>
