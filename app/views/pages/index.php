d8df4cfcfdf1cd26ec52e98e1d8a5a1b70bdd3dd
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-05-05 13:58:21 +0000

    added some UX changes

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -6,7 +6,7 @@
             Featured
         </div>
         <div class="card-body">
-            <h5 class="card-title">Who are we ?</h5>
+            <h5 class="card-title">Who we are ?</h5>
             <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis illo voluptates
                 architecto aliquam distinctio vitae optio sint pariatur provident in, odit tenetur nostrum sapiente fuga
                 accusantium voluptas fugit blanditiis quibusdam!
@@ -29,21 +29,21 @@
     <!-- Gallery -->
     <div class="row">
         <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-            <img src="./public/img/istockphoto-1000887536-612x612.jpg" class="w-100 shadow-1-strong rounded mb-4"
+            <img src="../public/img/istockphoto-1000887536-612x612.jpg" class="w-100 shadow-1-strong rounded mb-4"
                 alt="" />
 
-            <img src="./public/img/w555mh8d5vjlzk18skxj.webp" class="w-100 shadow-1-strong rounded mb-4" alt="" />
+            <img src="../public/img/w555mh8d5vjlzk18skxj.webp" class="w-100 shadow-1-strong rounded mb-4" alt="" />
         </div>
 
         <div class="col-lg-4 mb-4 mb-lg-0">
-            <img src="./public/img/images.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="" />
+            <img src="../public/img/images.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="" />
 
-            <img src="./public/img/www.usnews.com.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="" />
+            <img src="../public/img/www.usnews.com.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="" />
         </div>
         <div class="col-lg-4 mb-4 mb-lg-0">
-            <img src="./public/img/imagees.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="" />
+            <img src="../public/img/imagees.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="" />
 
-            <img src="./public/img/UNI330896.JPG" class="w-100 shadow-1-strong rounded mb-4" alt="" />
+            <img src="../public/img/UNI330896.JPG" class="w-100 shadow-1-strong rounded mb-4" alt="" />
         </div>
     </div>
 
@@ -57,22 +57,22 @@
         <div class="cards d-flex justify-content-around">
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="card-title">front end dev</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quas officia deleniti est veniam, incidunt vel assumenda corrupti sit quod exercitationem itaque minima natus porro.</p>
                     <a href="#" class="btn btn-primary">find out more ?</a>
                 </div>
             </div>
             <div class="card " style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="card-title">back end dev</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quas officia deleniti est veniam, incidunt vel assumenda corrupti sit quod exercitationem itaque minima natus porro.</p>
                     <a href="#" class="btn btn-primary">find out more ?</a>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="card-title">Full track</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quas officia deleniti est veniam, incidunt vel assumenda corrupti sit quod exercitationem itaque minima natus porro.</p>
                     <a href="#" class="btn btn-primary">find out more ?</a>
                 </div>
             </div>
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -2,58 +2,58 @@
 <?php require APPROOT .'/views/inc/nav.php'; ?>
 
 <style>
+  .form-signin {
+    width: 100%;
+    height: 80vh;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+  }
 
+  .form-signin .checkbox {
+    font-weight: 400;
+  }
 
-.form-signin {
-  width: 100%;
-  height: 80vh;
-  max-width: 330px;
-  padding: 15px;
-  margin: auto;
-}
+  .form-signin .form-floating:focus-within {
+    z-index: 2;
+  }
 
-.form-signin .checkbox {
-  font-weight: 400;
-}
+  .form-signin input[type="email"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
 
-.form-signin .form-floating:focus-within {
-  z-index: 2;
-}
-
-.form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
- </style>
+  .form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+</style>
 <main class="form-signin">
-  <form>
-    
+  <div class="container-signin">
+    <form>
 
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    
-  </form>
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+    </form>
+  </div>
 </main>
 <?php require APPROOT .'/views/inc/footer.php';?>
