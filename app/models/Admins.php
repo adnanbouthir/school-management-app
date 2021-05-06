2c75d06a976d23de339503c9313535b433292510
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-05-06 12:31:23 +0000

    working on login functionality

--- app/models/Admins.php
+++ app/models/Admins.php
@@ -11,4 +11,23 @@ class Admins {
         $this->db->query("SELECT * FROM admins");
         return $this->db->resultSet();
     }
+    // login as an admin
+    public function LogIn($email, $password) {
+        $this->db->query('SELECT * FROM admins WHERE email_adress = :email');
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->single();
+        $dbpass = $row->password;
+        if ($dbpass == $password) {
+            return $row;
+    
+    }else {
+        return false;
+    }
+}
+    public function findUser($email) {
+        $this->db->query('SELECT * FROM admins WHERE email_adress = :email');
+        $this->db->bind(':email', $email);
+        $row =$this->db->single();
+    }
 }
