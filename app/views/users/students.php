b12bbb8c09d3b887c3adc88ea2d7297731fdcc5b
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-05-06 18:50:36 +0000

    worked on students view

---
+++ app/views/users/students.php
@@ -0,0 +1,25 @@
+<h1 class="my-sm-5">Students</h1>
+
+<!-- contact list -->
+<h2 class="my-sm-4">Students list:</h2>
+<?php
+    if ($data) {
+        foreach ($data as $value) {
+?>
+            <hr>
+            <div class="row">
+                <p class="col-lg-1 font-weight-bold"><?php echo ' first name <br>' . $value->first_name; ?></p>
+                <p class="col-lg-2"><?php echo  'last name <br>' . $value->last_name; ?></p>
+                <p class="col-lg-2"><?php echo 'email adress <br>' . $value->email_adress; ?></p>
+                <p class="col-lg-6"><?php echo 'is admin <br>' . $value->is_admin; ?></p>
+                <div class="col-lg-1 d-flex flex-column">
+                    <a href="#">Update</a>
+                    <a href="#">Delete</a>
+                </div>
+            </div>
+<?php
+        }
+    }else{
+        echo "<p>No Students.</p>";
+    }
+?>
