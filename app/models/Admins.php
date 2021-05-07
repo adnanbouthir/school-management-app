d908b8e7622a5f8be1d11b2de02443e640efa4d3
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-05-07 18:48:26 +0000

    refactoring many things and starting from scratch

--- app/models/Admins.php
+++ app/models/Admins.php
@@ -1,53 +1,53 @@
 <?php 
-class Admins {
-    private $db;
+// class Admins {
+//     private $db;
 
-    public function __construct()
-    {
-        $this->db = new Database;
-    }
-    // show admins
-    public function showAdmins() {
-        $this->db->query("SELECT * FROM admins");
-        return $this->db->resultSet();
-    }
-    public function showStudents() {
-        $this->db->query("SELECT students.id, students.first_name AS student, students.last_name AS student_last, students.gender, students.class, parents.first_name, parents.last_name FROM `students` LEFT JOIN `parents` ON students.id = parents.student_id");
-        return $this->db->resultSet();
-    }
-    public function showProfessors() {
-        $this->db->query("SELECT * FROM professors");
-        return $this->db->resultSet();
-    }
-    public function showParents() {
-        $this->db->query("SELECT * FROM parents");
-        return $this->db->resultSet();
-    }
+//     public function __construct()
+//     {
+//         $this->db = new Database;
+//     }
+//     // show admins
+//     public function showAdmins() {
+//         $this->db->query("SELECT * FROM admins");
+//         return $this->db->resultSet();
+//     }
+//     public function showStudents() {
+//         $this->db->query("SELECT students.id, students.first_name AS student, students.last_name AS student_last, students.gender, students.class, parents.first_name, parents.last_name FROM `students` LEFT JOIN `parents` ON students.id = parents.student_id");
+//         return $this->db->resultSet();
+//     }
+//     public function showProfessors() {
+//         $this->db->query("SELECT * FROM professors");
+//         return $this->db->resultSet();
+//     }
+//     public function showParents() {
+//         $this->db->query("SELECT * FROM parents");
+//         return $this->db->resultSet();
+//     }
 
-    // login as an admin
-    public function LogIn($email, $password) {
-        $this->db->query('SELECT * FROM admins WHERE email_adress = :email');
-        $this->db->bind(':email', $email);
+//     // login as an admin
+//     public function LogIn($email, $password) {
+//         $this->db->query('SELECT * FROM admins WHERE email_adress = :email');
+//         $this->db->bind(':email', $email);
 
-        $row = $this->db->single();
-        $dbpass = $row->password;
-        if ($dbpass == $password) {
-            return $row;
+//         $row = $this->db->single();
+//         $dbpass = $row->password;
+//         if ($dbpass == $password) {
+//             return $row;
     
-    }else {
-        return false;
-    }
-}
-    public function findUser($email) {
-        $this->db->query('SELECT * FROM admins WHERE email_adress = :email');
-        $this->db->bind(':email', $email);
-        $row =$this->db->single();
-            // check row 
+//     }else {
+//         return false;
+//     }
+// }
+//     public function findUser($email) {
+//         $this->db->query('SELECT * FROM admins WHERE email_adress = :email');
+//         $this->db->bind(':email', $email);
+//         $row =$this->db->single();
+//             // check row 
 
-        if ($this->db->RowCount() > 0) {
-            return true;
-        }else {
-            return false;
-        }
-    }
-}
+//         if ($this->db->RowCount() > 0) {
+//             return true;
+//         }else {
+//             return false;
+//         }
+//     }
+// }
--- app/views/users/students.php
+++ app/views/users/students.php
@@ -6,12 +6,26 @@
 <h2 class="my-sm-4">Students list:</h2>
 <?php
     if ($data) {
+        
         foreach ($data as $value) {
 ?>
             <hr>
-            <div class="row table"> 
-                <p class="col-lg-1 font-weight-bold"><?php echo ' serial num <br>' . $value->id; ?></php>
-                <p class="col-lg-1 font-weight-bold"><?php echo ' first name <br>' . $value->student; ?></php>
+            <table class="row table table-striped">
+
+            <thead>
+            <td>id</td>
+            <td>first name</td>
+            <td>last name</td>
+            <td>gender</td>
+            <td>class</td>
+            </thead> 
+            <tr><td><?php echo  $value->id; ?></php></td>
+            <td><?php echo $value->student; ?></php></td>
+            <td><?php echo  $value->student_last; ?></td>
+            <td><?php echo  $value->gender; ?></td>
+            <td><?php echo   $value->class; ?></td></tr>
+                <p class="col-lg-1 font-weight-bold"><?php echo ' serial num <br>' . $value->id; ?></p>
+                <p class="col-lg-1 font-weight-bold"><?php echo ' first name <br>' . $value->student; ?></p>
                 <p class="col-lg-2"><?php echo  'last name <br>' . $value->student_last; ?></p>
                 <p class="col-lg-2"><?php echo 'gender <br>' . $value->gender; ?></p>
                 <p class="col-lg-6"><?php echo 'class <br>' . $value->class; ?></p>
@@ -22,10 +36,10 @@
                 <p class="col-lg-1 font-weight-bold"><?php echo ' last name <br>' . $value->last_name; ?></p>
                 </div>
 
-                <div class="col-lg-1 d-flex flex-column">
+                <table class="col-lg-1 d-flex flex-column">
                     <a href="#">Update</a>
                     <a href="#">Delete</a>
-                </div>
+                </table>
             
 <?php
         }
