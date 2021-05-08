ca4ae88583f58df858770b512831623b1b1b6a75
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-05-08 19:52:47 +0000

    enhancement

--- app/controllers/Students.php
+++ app/controllers/Students.php
@@ -118,4 +118,95 @@ class Students extends Controller {
 
         
     }
+    public function edit($id) {
+
+        //  check if form submitted 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            
+
+            
+            
+
+            // init data 
+            $data =  [
+                'first_name' => trim($_POST['first_name']),
+                'last_name' => trim($_POST['last_name']),
+                'gender' =>$_POST['gender'],
+                'class' => trim($_POST['class']),
+                'adress' => trim($_POST['adress']),
+                'date_of_birth' => $_POST['date_of_birth'],
+                'email' => trim($_POST['email']),
+                'first_name_err' => '',
+                'last_name_err' => '',
+                'class_err' => '',
+                'adress_err' => '',
+                'email_err' => '',
+                'birth_err' => ''
+            ];
+
+            // validate name
+
+            if (empty($data['first_name'])) {
+                $data['first_name_err'] = 'Please enter a name';
+            }
+            if (empty($data['last_name'])) {
+                $data['last_name_err'] = 'Please enter a name';
+            }
+
+            // validate gender/class
+            if (empty($data['gender'])) {
+                $data['class_err'] = 'Please enter a gender';
+            }
+            if (empty($data['class'])) {
+                $data['class_err'] = 'Please enter a class';
+            }
+
+            // validate email and adress
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter an email';
+            }
+            if (empty($data['adress'])) {
+                $data['adress_err'] = 'Please enter an address';
+            }
+
+            // make sure there is no more errors
+
+            if(empty($data['first_name_err']) && empty($data['last_name_err']) && empty($data['class_err']) && empty($data['email_err']) && empty($data['adress_error'])) {
+                //  validated
+
+                if ($this->studentModel->addStudent($data)) {
+                    flash('student_message', 'Student Added');
+                    redirect('/students');
+                }else {
+                    die('something went wrong');
+                }
+            }else {
+                // load view with errors
+
+                $this->view('students/add', $data);
+            }
+
+
+        }else {
+            $data =  [
+                'first_name' => '',
+                'last_name' => '',
+                'gender' => '',
+                'class' => '',
+                'parents' =>'',
+                'adress' => '',
+                'date_of_birth' => '',
+                'email' => '',
+                'first_name_err' => '',
+                'last_name_err' => '',
+                'class_err' => '',
+                'adress_err' => '',
+                'email_err' => '',
+                'birth_err' => ''
+            ];
+    
+            $this->view('students/edit ', $data);
+        }
+    }
+
 }
