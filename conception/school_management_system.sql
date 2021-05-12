01f49bbd80ed83b772344fa792bbaad86b19aa54
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-05-12 13:56:28 +0000

    added sql file and modeling diagrams

--- conception/school_management_system.sql
+++ conception/school_management_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2021 at 05:01 PM
+-- Generation Time: May 12, 2021 at 03:47 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -33,7 +33,7 @@ CREATE TABLE `admins` (
   `last_name` varchar(100) NOT NULL,
   `email_adress` varchar(150) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `is_admin` tinyint(4) NOT NULL
+  `is_admin` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -41,8 +41,11 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `first_name`, `last_name`, `email_adress`, `password`, `is_admin`) VALUES
-(1, 'adnan', 'bouthir', 'adnanbouthir@gmail.com', '123456', 1),
-(3, 'Youssef', 'Ouadid', 'Youadid@admin.com', '123456', 1);
+(4, 'adnan', 'bouthir', 'adnanbouthir@gmail.com', '$2y$10$wFqdhbSlUOF9oIZf6JDnq.kw14d26EZ4Z2TXZ3uXl2JLEhtdg22eG', 1),
+(7, 'youssef', 'bhl', 'bhl@gmail.com', '$2y$10$yYjowu9hO8IsdymN8OikY.cfVEw3kWbXY/jljGALj3eSBkg1yzryy', 1),
+(8, 'Youssef', 'DIOUANI', 'diouani@gmail.com', '$2y$10$MyjZaPeMQEaHQ/n.4NCnB.n2zsaOrqfwL.Ie9c2zzXxxbcLbJPoaW', 1),
+(9, 'adam', 'bou', 'adam@gmail.com', '$2y$10$1s9W4yRXdVLyqtl9jEJ3quVPb1DsyccmYguR/32Hh9WTuRp3BCp92', 1),
+(10, 'test', 'admin', 'admin@gmail.com', '$2y$10$9pM5W46EkdYYJMsacBjrI.WZWehC0UsSiRJckseagnaR6rcyNLcky', 1);
 
 -- --------------------------------------------------------
 
@@ -57,8 +60,17 @@ CREATE TABLE `parents` (
   `gender` varchar(100) NOT NULL,
   `job` varchar(255) NOT NULL,
   `adress` varchar(255) NOT NULL,
-  `phone` varchar(100) NOT NULL
+  `phone` varchar(100) NOT NULL,
+  `student_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `parents`
+--
+
+INSERT INTO `parents` (`id`, `first_name`, `last_name`, `gender`, `job`, `adress`, `phone`, `student_id`) VALUES
+(7, 'abdelaziz', 'bouthir', 'male', 'retired', '89 amerchich marrakech', '0661581818', 16),
+(9, 'mohamed', 'nassifi', 'male', 'butcher', 'SYBA 35', '747379302', 19);
 
 -- --------------------------------------------------------
 
@@ -76,6 +88,15 @@ CREATE TABLE `professors` (
   `phone` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `professors`
+--
+
+INSERT INTO `professors` (`id`, `first_name`, `last_name`, `gender`, `Class`, `subject`, `phone`) VALUES
+(1, 'Youssef', 'Ouadid', 'male', 'NAMEK', 'fullstack dev', '0663368386'),
+(2, 'bouchra', 'marzak', 'female', 'JAVA', 'java/jee', '0663368386'),
+(3, 'Youssef', 'Chouni', 'male', 'storm troopers', 'fullstack dev', '0663368386');
+
 -- --------------------------------------------------------
 
 --
@@ -91,8 +112,18 @@ CREATE TABLE `students` (
   `parents` varchar(255) NOT NULL,
   `adress` varchar(255) NOT NULL,
   `date_of_birth` date NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `teacher` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `first_name`, `last_name`, `gender`, `class`, `parents`, `adress`, `date_of_birth`, `email`, `teacher`) VALUES
+(16, 'adnan', 'bouthir', 'male', 'NAMEK', '', '89 amerchich marrakech', '1998-10-04', 'adnanbouthir@gmail.com', 1),
+(17, 'youssef', 'DIOUANI', 'male', 'NAMEK', '', 'casa', '1996-09-12', 'diouani.youssef@gmail.com', 1),
+(19, 'hakim', 'nasifi', 'male', 'JAVA', '', 'SYBA', '1996-12-24', 'hakim@gmail.com', 2);
 
 --
 -- Indexes for dumped tables
@@ -108,7 +139,8 @@ ALTER TABLE `admins`
 -- Indexes for table `parents`
 --
 ALTER TABLE `parents`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_parent_student` (`student_id`);
 
 --
 -- Indexes for table `professors`
@@ -130,25 +162,35 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `parents`
 --
 ALTER TABLE `parents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `professors`
 --
 ALTER TABLE `professors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `parents`
+--
+ALTER TABLE `parents`
+  ADD CONSTRAINT `fk_parent_student` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
